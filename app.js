1acbb186c846ceed6719733070e69f9b732a5f2e
RBJ40 <celinechan268@gmail.com>
2024-10-01 12:29:35 -0400

    draft

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let DOMSelectors = {};
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  description: document.querySelector(".card-desc"),
+  items: document.querySelectorAll("li"),
+};
 function addElement(input) {}
+const header = document.querySelector("h1");
+console.log(header.parentElement);
+console.log(DOMSelectors.description);
+console.log(DOMSelectors.items);
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(event.target);
+});
