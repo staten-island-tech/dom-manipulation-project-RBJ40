2a737404c11aa081f1873f0808e440f4fd68c4cf
RBJ40 <celinechan268@gmail.com>
2024-10-09 12:25:28 -0400

    draft

--- app.js
+++ app.js
@@ -2,13 +2,43 @@ const DOMSelectors = {
   header: document.querySelector("h1"),
   description: document.querySelector(".card-desc"),
   items: document.querySelectorAll("li"),
+  button: document.querySelectorAll(".button"),
+  form: document.querySelectorAll(".form"),
+  container: document.querySelectorAll(".container"),
 };
+DOMSelectors.button.addEventListener("click", function (event) {
+  console.log(event.target.parentElement);
+  event.target.parentElement.style.backgroundColor = "red";
+});
 function addElement(input) {}
 const header = document.querySelector("h1");
 console.log(header.parentElement);
 console.log(DOMSelectors.description);
 console.log(DOMSelectors.items);
-form.addEventListener("submit", function (event) {
+DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(event.target);
+  console.log(document.querySelector("input").value);
 });
+
+const item = document.querySelectorAll("li");
+const items = Array.from(item);
+items.forEach((el) => (el.style.color = "red"));
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) =>
+  button.addEventListener("click", function (event) {
+    console.log(event.target.textContent);
+  })
+);
+let album = {
+  name: "Abbey Road",
+};
+DOMSelectors.container.insertAdjacentHTML(
+  "beforeend",
+  `<div class="card">${album.name}</div>`
+);
+
+//event listener for form
+//get values from form for widget object
+//create the card and insert it
+//find remove buttons and add event listeners
