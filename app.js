67481c3137bffa3d13c7d09b923b93afa16be457
RBJ40 <celinechan268@gmail.com>
2024-10-11 12:28:38 -0400

    draft

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const item = document.querySelectorAll("li");
 const items = Array.from(item);
 items.forEach((el) => (el.style.color = "red"));
 
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".button");
 buttons.forEach((button) =>
   button.addEventListener("click", function (event) {
     console.log(event.target.textContent);
